6558abb68d6a823467a7df420ad55cfeb6211195
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2023-12-14 00:11:58 -0300

    Initial commit, undestanding Dictionary Comprehension

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
+# here we have a list of names
 names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freedie']
 
 print(names)
 
+                 #{new_key:new_value for new_key in names}
 student_scores = {student:random.randint(1,100) for student in names}
 
 print(student_scores)
