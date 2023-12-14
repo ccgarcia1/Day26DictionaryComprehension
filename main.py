bbe8d5ba7783ad269f3d82866335bfee2147b538
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2023-12-14 00:21:06 -0300

    Initial commit, undestanding Dictionary Comprehension

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freedie']
 
 print(names)
 
-                 #{new_key:new_value for new_key in names}
+                 # {new_key:new_value for new_key in names}
 student_scores = {student:random.randint(1,100) for student in names}
 
 print(student_scores)
+
+# syntax: passed_students = {new_key: new_value for (key, value) in dictionary.items()}
+passed_students = {student: score for (student, score) in student_scores.items() if score >= 60}
+
+print(passed_students)
