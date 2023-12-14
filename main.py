65cec3ab1ce24eb1f53cf7f783f544555955b656
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2023-12-14 00:27:58 -0300

    Initial commit, undestanding Dictionary Comprehension

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freedie']
 print(names)
 
                  # {new_key:new_value for new_key in names}
-student_scores = {student:random.randint(1,100) for student in names}
+student_scores = {student: random.randint(1, 100) for student in names}
 
 print(student_scores)
 
